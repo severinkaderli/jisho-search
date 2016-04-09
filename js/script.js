b98fceb0431ba233816d62f61e30a901b12b2d8b
Severin Kaderli <severin.kaderli@gmail.com>
2016-04-09 21:10:15 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Creating the context menu.
+ */
+chrome.contextMenus.create({
+    title: "Search on jisho.org: %s", 
+    contexts:["selection"], 
+    onclick: search,
+});
+
+/**
+ * The Functions to open the search results in a new tab.
+ */
+function search(info, tab) {
+    chrome.tabs.create({ 
+        url: "http://jisho.org/search/" + info.selectionText
+    })
+}
+
